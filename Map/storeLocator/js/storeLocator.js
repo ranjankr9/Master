8b89887025f64a28e37c28254101d974e9bc6d07
ranjankr9 <ranjankr9@gmail.com>
2017-06-14 23:06:18 +0530

    Add files via upload

--- Map/storeLocator/js/storeLocator.js
+++ Map/storeLocator/js/storeLocator.js
@@ -1,76 +1,221 @@
 var baiduMap = {} || window.baiduMap;
-var baiduStoreMapData = [] || window.baiduStoreMapData;
-var baiduMapContainer = 'map_canvas';
-var baiduMapZoomLevel = 15;
-var defaultLat = 116.404;
-var defaultLong = 39.915;
-
-
-$(document).ready(function(){
-	loadMap();
-});
-
-
-/**
- * Server call to get the store details
- */
-function getStoreData(){
-	/*$.ajax({
-      type: "POST",
-      url: "../html/response.html",
-      success: function(resultData){
-          alert("Save Complete : " + resultData);
-          storeMapData = resultData;
-      },
-      error : function(errorResponse) {
-    	  alert('error occured : ' + errorResponse);
-      }
- });
- return storeMapData;
- */
-}
-
-/**
- * This function helps in creating common configuration and setting involved in the map generation
- */
-function initBaiduMapConfig() {
-	baiduMap = new BMap.Map(baiduMapContainer);
-	var point = new BMap.Point(defaultLat, defaultLong);
-	baiduMap.centerAndZoom(point, baiduMapZoomLevel);
-	window.baiduMap = baiduMap;
-}
-
-/**
- * This function helps in loading map in the map Container
- */
-function loadMap() {
-	initBaiduMapConfig();
+	var baiduStoreMapData = [] || window.baiduStoreMapData;
+	var baiduMapContainer = 'map_canvas';
+	var baiduMapZoomLevel = 15;
+	var defaultLat = 39.921988;
+	var defaultLong = 116.417854;
 	
-	/**
-	 * 1. Get Server data from Server
-	 * 2. Iterate over storeData and populate map
-	 */
+	var storesMarker = [];
+	var showRoomsMarker = [];
+	var outletsMarker = [];
+	var storesMarkerInfoWindowContents = [];
+	var showRoomsMarkerInfoWindowContents = [];
+	var outletsMarkerInfoWindowContents = [];
+
+	var storeMarkerIcon = '../js/mapIconImages/stores-location.png';
+	var showRoomsMarkerIcon = '../js/mapIconImages/showrooms-location.png';
+	var outletsMarkerIcon = '../js/mapIconImages/outlets-location.png';
+	var downArrorMarkerIcon = '../js/mapIconImages/down-arrow.png';
+	var closeMarkerIcon = '../js/mapIconImages/close.gif';
 	
-	//remove above hardCoding once server code is in place or custom store data provided by client in any form 
-	//storeMapData = getStoreData();
-	baiduStoreMapData = [{"longitude":116.34455821640596,"latitude":39.92805897486837},{"longitude":116.42014555403168,"latitude":39.90786802906522},{"longitude":116.4185905604133,"latitude":39.91800989459023},{"longitude":116.38085396878016,"latitude":39.93100637717548},{"longitude":116.42591613949898,"latitude":39.91162012284861},{"longitude":116.36005939949703,"latitude":39.92468954453223},{"longitude":116.36649123309752,"latitude":39.907918426832325},{"longitude":116.4302960828572,"latitude":39.91414002231574},{"longitude":116.33653853722959,"latitude":39.92252752030005},{"longitude":116.41794939934924,"latitude":39.908488577147516},{"longitude":116.41996497506523,"latitude":39.917056766365796},{"longitude":116.3494336999557,"latitude":39.92921395795512},{"longitude":116.3640331775793,"latitude":39.93229851896867},{"longitude":116.42396612888842,"latitude":39.912911462470774},{"longitude":116.34289374766483,"latitude":39.92972453979731},{"longitude":116.35375750734305,"latitude":39.910262310969934},{"longitude":116.37718320073068,"latitude":39.91014754195528},{"longitude":116.36840465119838,"latitude":39.92291471111395},{"longitude":116.42482514477634,"latitude":39.90979051777687},{"longitude":116.4103603329159,"latitude":39.90945976001113},{"longitude":116.34443364808553,"latitude":39.93245060847492},{"longitude":116.37382003697583,"latitude":39.91526466908088},{"longitude":116.40171776593368,"latitude":39.930939789357964},{"longitude":116.36421981807327,"latitude":39.91201613084136},{"longitude":116.36839923767323,"latitude":39.921307434122916}];
-	$.each(baiduStoreMapData, function(key, storeInfo){
-		addBaiduMarker(storeInfo.latitude, storeInfo.longitude);
+	$(document).ready(function(){
+		loadMap();
 	});
-	
-}
 
-/**
- * This function helps in adding the marker on the map based on the points information requested
- * @param latitude
- * @param longitude
- */
-function addBaiduMarker(latitude, longitude) {
-	var baiduMarkerPoint = new BMap.Point(longitude, latitude);
-	var baiduMarker = new BMap.Marker(baiduMarkerPoint);
-	baiduMap.addOverlay(baiduMarker);
-}
 
-function createInfoBox() {
+	$(document).ready(function(){
+		
+		$("input[name='store_type'][type='checkbox'][id='stores']").change(function(){
+			if( $(this).prop('checked') == true ){
+				showMarkers(storesMarker, storesMarkerInfoWindowContents);
+			} else {
+				hideMarkers(storesMarker);
+			}
+		});
+		
+		$("input[name='store_type'][type='checkbox'][id='showrooms']").change(function(){
+			if( $(this).prop('checked') == true ){
+				showMarkers(showRoomsMarker, showRoomsMarkerInfoWindowContents);
+			} else {
+				hideMarkers(showRoomsMarker);
+			}
+		});
+		
+		$("input[name='store_type'][type='checkbox'][id='outlets']").change(function(){
+			if( $(this).prop('checked') == true ){
+				showMarkers(outletsMarker, outletsMarkerInfoWindowContents);
+			} else {
+				hideMarkers(outletsMarker);
+			}
+		});
+		
+		$("input[name='unique-name'][type='checkbox'][id='current-location']").change(function(){
+			if( $(this).prop('checked') == true ){
+				alert('Need to show Current Location marker');
+			} else {
+				alert('Need to hide Need to hide Current Location marker');
+			}
+		});
+		
+		
+		
+	});
+
+	/**
+	 * Server call to get the store details
+	 */
+	function getStoreData(){
+		/*$.ajax({
+	      type: "POST",
+	      url: "../html/response.html",
+	      success: function(resultData){
+	          alert("Save Complete : " + resultData);
+	          storeMapData = resultData;
+	      },
+	      error : function(errorResponse) {
+	    	  alert('error occured : ' + errorResponse);
+	      }
+	 });
+	 return storeMapData;
+	 */
+	}
+
+	/**
+	 * This function helps in creating common configuration and setting involved in the map generation
+	 */
+	function initBaiduMapConfig() {
+		baiduMap = new BMap.Map(baiduMapContainer);
+		var point = new BMap.Point(defaultLong, defaultLat);
+		baiduMap.centerAndZoom(point, baiduMapZoomLevel);
+		baiduMap.enableScrollWheelZoom();
+		baiduMap.enableContinuousZoom();	
+		//baiduMap.enablePinchToZoom();	
+		window.baiduMap = baiduMap;
+	}
+
+
+
+	/**
+	 * This function helps in loading map in the map Container
+	 */
+	function loadMap() {
+		initBaiduMapConfig();
+		
+		/**
+		 * 1. Get Server data from Server
+		 * 2. Iterate over storeData and populate map
+		 */
+		
+		//remove above hardCoding once server code is in place or custom store data provided by client in any form 
+		//storeMapData = getStoreData();
+		baiduStoreMapData = [{"longitude":116.417854,"latitude":39.921988,"type":"store","storeName":"Store A","StoreURL":"","addressLine1":"Store A Line 1","addressLine2":"Store A Line 2","formattedStoreContactNo":"(123) 459-1234","storeDescription":"Store A Description"},{"longitude":116.406605,"latitude":39.921585,"type":"showroom","storeName":"ShowRoom A","StoreURL":"","addressLine1":"ShowRoom A Line 1","addressLine2":"ShowRoom A Line 2","formattedStoreContactNo":"(123) 459-1236","storeDescription":"ShowRoom A Description"},{"longitude":116.412222,"latitude":39.912345,"type":"outlets","storeName":"Outlets B","StoreURL":"","addressLine1":"Outlets A Line 1","addressLine2":"Outlets A Line 2","formattedStoreContactNo":"(123) 459-1238","storeDescription":"Outlets A Description"}];
+		$.each(baiduStoreMapData, function(key, storeInfo){
+			addBaiduMarker(storeInfo);
+		});
+		
+	}
+
+	/**
+	 * This function helps in adding the marker on the map based on the points information requested
+	 * @param latitude
+	 * @param longitude
+	 */
+	function addBaiduMarker(storeInfo) {
+		var latitude = storeInfo.latitude;
+		var longitude = storeInfo.longitude;
+		var markerType = storeInfo.type;
+		var addedMarkers;
+		var infoWindowContent;
+		var storeName = storeInfo.storeName;
 	
-}
+		infoWindowContent = createInfoBoxContent(storeInfo);
+		if(markerType === 'store') {
+			storesMarkerInfoWindowContents.push(infoWindowContent);
+			addedMarkers = createAndAddBaiduMarkerToMap(longitude, latitude, storeMarkerIcon, storeName);
+			storesMarker.push(addedMarkers);
+		} else if(markerType === 'showroom') {
+			showRoomsMarkerInfoWindowContents.push(infoWindowContent);
+			addedMarkers = createAndAddBaiduMarkerToMap(longitude, latitude, showRoomsMarkerIcon, storeName);
+			showRoomsMarker.push(addedMarkers);
+		} else if ( markerType === 'outlets') {
+			outletsMarkerInfoWindowContents.push(infoWindowContent);
+			addedMarkers = createAndAddBaiduMarkerToMap(longitude, latitude, outletsMarkerIcon, storeName);
+			outletsMarker.push(addedMarkers);
+		}
+
+		//Click event handler for showing info window for each marker
+		showInfoWindow(addedMarkers, infoWindowContent);
+		
+	}
+
+	/**
+	 * This function helps in creating and adding marker category specific to the BAIDU map
+	 * @param longitude
+	 * @param latitude
+	 * @param markerImage
+	 * @returns {BMap.Marker}
+	 */
+	function createAndAddBaiduMarkerToMap(longitude, latitude, markerImage, storeName) {
+		
+		var baiduMarkerPoint;
+		var baiduIcon;
+		var baiduMarker;
+		
+		baiduMarkerPoint = new BMap.Point(longitude, latitude);
+		baiduIcon = new BMap.Icon(markerImage, new BMap.Size(300,157));
+		baiduMarker = new BMap.Marker(baiduMarkerPoint, {icon:baiduIcon});
+		baiduMarker.setTitle(storeName);
+		baiduMap.addOverlay(baiduMarker);
+		return baiduMarker;
+	}
+
+	/**
+	 * This helps in showing markers at run time
+	 * @param baiduMarkers
+	 */
+	function showMarkers(baiduMarkers, infoWindoContentData) {
+		baiduMarkers.forEach(function(marker, index) {
+			baiduMap.addOverlay(marker);
+			showInfoWindow(marker, infoWindoContentData[index]);
+		});
+	}
+
+	/**
+	 * This helps in hiding markers at run time
+	 * @param baiduMarkers
+	 */
+	function hideMarkers(baiduMarkers) {
+		baiduMarkers.forEach(function(marker) { 
+			baiduMap.removeOverlay(marker);
+		});
+	}
+	
+	function createInfoBoxContent(storeInfo) {
+		
+		var storeName = storeInfo.storeName;
+		var storeURL = storeInfo.StoreURL;
+		var addressLine1 = storeInfo.addressLine1;
+		var addressLine2 = storeInfo.addressLine2;
+		var formattedStoreContactNo = storeInfo.formattedStoreContactNo;
+		var storeDescription = storeInfo.storeDescription;
+		
+		var content = '<div class="lulu-infobox">'+
+        '<h1><a href="' + storeURL + '">' + storeName + '</a></h1>'+
+        '<h2>contact</h2>'+ '<p class="address">' + addressLine1 + "<br>" + addressLine2 + '</p>'+ '<br>'+
+        '<a href="tel:' + formattedStoreContactNo + '">' + formattedStoreContactNo + '</a>' + + '<br>'+
+        '<h3>' + storeDescription + '</h3>' +
+        '</div>';
+        
+        return content;
+	}
+	
+	//var opts = {width:300, height:100};
+	
+	function showInfoWindow(addedMarkers, infoWindowContent) {
+        addedMarkers.addEventListener('click', function(event){
+		    var target = event.target;
+			var point = new BMap.Point(target.getPosition().lng, target.getPosition().lat);
+			var infoWindow = new BMap.InfoWindow(infoWindowContent);
+			this.openInfoWindow(infoWindow, point);
+        });
+	}
